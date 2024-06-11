4be1fe34c755043805a6447c41242be4ebf00683
ophewinx <winterhoffophelie@gmail.com>
2024-06-11 15:08:39 +0200

    premier algo

--- src/main/java/iut/sae/algo/Algo.java
+++ src/main/java/iut/sae/algo/Algo.java
@@ -1,10 +1,31 @@
 package iut.sae.algo;
 
-
 public class Algo{
     public static String RLE(String in){
-        // Provide your algo here
-        return "NotYetImplemented";
+        if (in.isEmpty()){
+            return "";
+        }
+        StringBuilder str_compresse= new StringBuilder();
+        char chr_lettre=in.charAt(0);
+        int count=1;
+
+        for (int i=1; i<in.length();i++){
+            if (in.charAt(i)==chr_lettre){
+                count++;
+                if(count==10){
+                    str_compresse.append(9);
+                    str_compresse.append(chr_lettre);
+                    count=1;
+                }
+            }else{
+                str_compresse.append(count);
+                str_compresse.append(chr_lettre);
+                chr_lettre=in.charAt(i);
+                count=1;
+            }
+        }
+        str_compresse.append(count).append(in.charAt(in.length()-1));
+        return str_compresse.toString();
     }
 
     public static String RLE(String in, int iteration) throws AlgoException{
